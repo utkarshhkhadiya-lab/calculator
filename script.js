24329f268b2f9296e08b9292e5d6d3cde47d1aeb
utkarshhkhadiya-lab <utkarshhkhadiya@gmail.com>
2026-09-06 19:47:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const input = document.getElementById("inputbox");
+const buttons =document.querySelectorAll("button");
+let string ="";
+// let arr =Array.from(buttons);
+buttons.forEach(button=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=="="){
+            string=eval(string);
+            input.value=string;
+        }    
+        else if(e.target.innerHTML=="AC"){
+            string="";
+            input.value=string;
+        }    
+        else if(e.target.innerHTML=="DEL"){
+            string=string.slice(0,-1);
+            input.value=string;
+        }
+        else{
+        string+=e.target.innerHTML;
+        input.value =string;
+        }
+    }) 
+})
